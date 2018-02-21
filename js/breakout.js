424e47b96d92c2287ba2b7e68cedb40ec0c9c86a
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-02-21 13:45:51 +0000

    Starting The Game
    Setup the ball and fps

--- js/breakout.js
+++ js/breakout.js
@@ -1,26 +1,20 @@
+//canvas setup
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 100, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+//set start point
+var x = canvas.width/2;
+var y = canvas.height-30;
 
-ctx.beginPath();
-ctx.arc(240,160,20,0,Math.PI*2,false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+//draw ball
+function draw() {
+	ctx.beginPath();
+	ctx.arc(x,y,10,0,Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	
+}
 
-ctx.beginPath();
-ctx.rect(160,10,100,40);
-ctx.strokeStyle = "rgba(0,0,255,0.5)";
-ctx.stroke();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(420,260,50,50);
-ctx.fillStyle = "yellow";
-ctx.fill();
-ctx.closePath;
+//calls draw function every 10ms
+setInterval(draw,10);
