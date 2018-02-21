d038af872a0ef10b849c6d8574446c7f36e9f08e
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-02-21 13:39:54 +0000

    Exercise
    Yellow Square

--- js/breakout.js
+++ js/breakout.js
@@ -18,3 +18,9 @@ ctx.rect(160,10,100,40);
 ctx.strokeStyle = "rgba(0,0,255,0.5)";
 ctx.stroke();
 ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(420,260,50,50);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.closePath;
