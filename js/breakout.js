5b3a5f815884a8dbf0eaef7f49f78c301a1dbf37
conorcallanan <35769284+conorcallanan@users.noreply.github.com>
2018-02-28 13:47:51 +0000

    draw the paddle

--- js/breakout.js
+++ js/breakout.js
@@ -3,42 +3,87 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
 
+//paddle vars
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
 //set start point
 var x = canvas.width/2;
 var y = canvas.height-30;
-
 var dx = 2;
 var dy = -2;
+var ballColour = "#0095DD";
+
+var rightPressed = false;
+var leftPressed = false;
 
 //draw ball
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x,y,ballRadius,0,Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+	ctx.fillStyle = ballColour;
+	ctx.fill();
+	ctx.closePath();
+}
+
+//draw paddle
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
 }
 
 function draw() {
-	ctx.clearRect(0,0,480,320);
+	ctx.clearRect(0,0,canvas.width,canvas.height);
 	drawBall();
+	drawPaddle();
 	x += dx;
 	y += dy;
 	
 	//making ball bounce and change colour
-	if(y + dy >= 320 || y+dy <= 0) {
+	if(y + dy > canvas.height-ballRadius || y+dy < ballRadius) {
 		dy = -dy;
-		ctx.fillStyle ="red";
-		ctx.fill();
+		
 	}
 	
-	if(x + dx == 470 || x+dx <= ballRadius) {
+	if(x + dx > canvas.width-ballRadius || x+dx < ballRadius) {
 		dx = -dx;
-		ctx.fillStyle = "red";
-		ctx.fill();
+	}
+	
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+		paddleX -= 7;
 	}
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
+
+
+
 //calls draw function every 10ms
-setInterval(draw,50);
+setInterval(draw,15);
 
